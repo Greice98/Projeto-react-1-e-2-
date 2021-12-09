895c6af786f51600af6f33a338850eabd94238c6
Greice98 <gracykellysilva.98@gmail.com>
2021-12-08 21:48:44 -0300

    projeto react 1 e 2

---
+++ tema_05_agenda/src/Components/TaskItem.jsx
@@ -0,0 +1,10 @@
+	const TaskItem = () => {  
+    return (  
+           <div className="task">  
+    	            <h3>Minha Tarefa</h3>  
+    	        </div>  
+    	    );  
+    	};  
+    	  
+    	export default TaskItem;
+    
